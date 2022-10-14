2e12de503f1b6931184ec40f0c16e2bcc8021bb0
advoidh-b <256.adwaith@gmail.com>
2022-10-14 12:07:04 +0530

    SC forenoon

--- scripts/regact.php
+++ scripts/regact.php
@@ -1,21 +1,21 @@
 <?php
+
 include('config.php');
-if(isset($_POST['signup']))
-{
-    $name=$_POST['name'];
-    $surname=$_POST['surname'];
-    $gender=$_POST['gender'];
-    $dob=$_POST['dob']; 
+
+if (isset($_POST['signup'])) {
+    $name=$_POST['username'];
     $email=$_POST['email'];
     $password=$_POST['password'];
-    $sql="INSERT INTO `tb_register` (`name`, `surname`, `gender`, `dob`, `email`, `password`) VALUES ('$name', '$surname', '$gender', '$dob', '$email', '$password')";
-    if(mysqli_query($conn, $sql))
-    {
-        header("location:../signup.html"); 
+    $sql="INSERT INTO `tb_register` (`username`, `email`, `password`) VALUES ('$username', '$email', '$password')";
+
+    if(mysqli_query($conn, $sql)) {
+        header("location:../forms/signup.html");
     }
     else
     {
-        echo "error".$sql."<br>".mysqli_error($conn);
+        echo mysqli_error($conn);
     }
+
 }
+
 ?>
