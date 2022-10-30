fd93e28b9ea80d0c88daff87636c8c7c235e345a
advoidh-b <256.adwaith@gmail.com>
2022-10-30 22:06:07 +0530

    Viewer.php: record viewing complete

--- scripts/viewer.php
+++ scripts/viewer.php
@@ -5,30 +5,70 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>U-Cred</title>
+    <style>
+        body {
+            font: 400 14px "Arial", "Tahoma", sans-serif;
+        }
+        table > tr, td {
+            border: 1px dotted #55f;
+            padding: 10px;
+        }
+        .del {
+            background: #d66;
+            padding: 8px 10px;
+            color: #fff;
+        }
+    </style>
 </head>
 <body>
+    <table>        
+        <tr>
+            <th>Id</th>
+            <th>Username</th>
+            <th>Emai</th>
+            <th>Password</th>
+        </tr>
     <?php 
-     echo "File Load Okay<br>"
-     $t_conn = new mysqli("localhost", "root", "", "user_creden");
+     echo "<br>";
+     $t_conn = new mysqli("localhost","root","","user_creden");
 
-     if($t_conn->connect_err) {
+     if($t_conn->connect_error) {
         echo "Connect Error";
      }
      else {
-        $v_query = "select id, username, email, password from tb_user";
+        $v_query = "SELECT id, username, email, password FROM tb_user";
         $q_out = $t_conn->query($v_query);
 
         if($q_out->num_rows > 0) {
 
             while($recd = $q_out->fetch_assoc()) {
-                echo $recd["id"]." ".$recd["username"]." ".$recd["email"];
+
+                $usid = $recd["id"];
+                $usn = $recd["username"];
+                $use = $recd["email"];
+                $usp = $recd["password"];
+
+                echo "<tr>";
+
+                echo "<td>".$usid."</td>";
+                echo "<td>".$usn."</td>";
+                echo "<td>".$use."</td>";
+                echo "<td>".$usp."</td>";
+                echo "<td>";
+                echo "<a href='#' class='del'>Delete</a>";
+                echo "</td>";
+                
+                echo "</tr>";
+                
             }
+    
         }
         else {
             echo "No results";
         }
      }
-     $t_conn->close();
+     
     ?>
+    </table>
 </body>
 </html>
