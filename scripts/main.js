295d32a6c2e26debcf033357a6d7776cbf940127
advoidh-b <256.adwaith@gmail.com>
2022-11-04 23:22:42 +0530

    23-22, Welcome page set

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,15 @@
+/* * Author: Advoidh B
+   * for: Travelogue(TM) */
+
 "use strict";
+
+/* Sidebar opening and closing */
+const navOpen = () => {
+    document.querySelector(".sidebar").style.width = '32%';
+};
+const navCls = () => {
+   let sideBar =  document.querySelector(".sidebar");
+   sideBar.style.width = 0;
+};
 
 
