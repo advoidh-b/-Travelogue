73672b3dcafa1bfc86376181eb5c11c6ff679721
advoidh-b <256.adwaith@gmail.com>
2022-11-05 11:59:57 +0530

    11-59 sunday

--- scripts/main.js
+++ scripts/main.js
@@ -5,11 +5,19 @@
 
 /* Sidebar opening and closing */
 const navOpen = () => {
-    document.querySelector(".sidebar").style.width = '32%';
+   let sidebar = document.querySelector(".sidebar");
+
+   if(window.innerWidth > 700) {
+      sidebar.style.width = "32%";
+   }
+   else {
+   sidebar.style.width = "100%";
+   }
 };
+
 const navCls = () => {
    let sideBar =  document.querySelector(".sidebar");
    sideBar.style.width = 0;
 };
 
-
+console.log(window.innerWidth, screen.width);
