5037c608ea028ae46b2c7d5bdb58c5d28baf0081
advoidh-b <256.adwaith@gmail.com>
2022-11-20 22:05:20 +0530

    script update: destination.html

--- scripts/destinations_.js
+++ scripts/destinations_.js
@@ -21,3 +21,37 @@ function change(n) {
         }
     }
 }
+
+function populatePage() {
+    let cntry = localStorage.getItem('cntry');
+    let c_code = localStorage.getItem('cCode');
+
+        let mT = document.getElementById('mainT');
+        let parsD = localStorage.getItem('cntry');
+        
+        let cntryStr = localStorage.getItem('cntry').toString(),
+            c_codeStr = localStorage.getItem('cCode').toString();
+
+         mT.textContent = cntryStr;
+
+         let imgarr = ["1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg"],
+             imgs = document.getElementsByClassName("img");
+
+            for(let i = 0; i <= imgs.length; ++i) {
+             
+             let showC = document.querySelector('.left');
+
+             if(c_codeStr == "sw") {
+              
+              showC.style.backgroundImage = "url('../images/sw/1.jpeg')";
+              imgs[i].src = "../images/sw/" + imgarr[i];
+                
+            }
+             if(c_code == "nwr") {
+                showC.style.backgroundImage = "url('../images/nwr/1.jpeg')";
+              imgs[i].src = "../images/nwr/" + imgarr[i];
+             }
+            }
+
+}
+populatePage();
--- scripts/hotel-script.js
+++ scripts/hotel-script.js
@@ -1,3 +1,8 @@
 "use strict";
 
- 
+function dest(cCode,cntry) {
+    window.location.assign("./dests/destination.html");
+    localStorage.setItem('cCode',cCode);
+    localStorage.setItem('cntry',cntry);
+    console.log(localStorage.getItem('cntry'),localStorage.getItem('cCode'));
+ }
